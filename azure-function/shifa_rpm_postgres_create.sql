e220085b435919bef81487ed7e9d5438a32acdcf
Hussnain Ahmed <hazznain@hotmail.com>
2020-10-06 00:27:42 +0300

    sql statements modiefied

--- azure-function/shifa_rpm_postgres_create.sql
+++ azure-function/shifa_rpm_postgres_create.sql
@@ -11,7 +11,7 @@ CREATE TABLE "devices_meta" (
 
 
 CREATE TABLE "users_meta" (
-	"user_id" SERIAL NOT NULL,
+	"user_id" VARCHAR NOT NULL,
 	"user_name" VARCHAR NOT NULL,
 	"address" VARCHAR,
 	"dob" DATE NOT NULL,
@@ -29,6 +29,8 @@ CREATE TABLE "device_rentals" (
 	"transaction_id" SERIAL NOT NULL,
 	"is_returned" BOOLEAN,
 	"returned_at" TIMESTAMP,
+	FOREIGN KEY (device_id) REFERENCES devices_meta (device_id),
+	FOREIGN KEY (rented_to_uid) REFERENCES users_meta (user_id),
 	CONSTRAINT "device_rentals_pk" PRIMARY KEY ("transaction_id")
 );
 
@@ -37,7 +39,7 @@ CREATE TABLE "device_rentals" (
 CREATE TABLE "rpm_cont_data" (
 	"recorded_at" TIMESTAMP NOT NULL,
 	"device_id" VARCHAR NOT NULL,
-	"rented_to_id" VARCHAR NOT NULL,
+	"rented_to_uid" VARCHAR NOT NULL,
 	"rented_to_name" VARCHAR NOT NULL,
 	"temperature" DECIMAL(2) NOT NULL,
 	"oxygen_saturation" smallint,
@@ -48,28 +50,44 @@ CREATE TABLE "rpm_cont_data" (
 
 
 
-CREATE TABLE "rpm_cont_data copy" (
+CREATE TABLE "rpm_non_cont_data" (
 	"recorded_at" TIMESTAMP NOT NULL,
 	"device_id" VARCHAR NOT NULL,
-	"rented_to_id" VARCHAR NOT NULL,
+	"rented_to_uid" VARCHAR NOT NULL,
 	"rented_to_name" VARCHAR NOT NULL,
 	"bp_systolic" smallint,
 	"bp_diastolic" smallint
 );
 
+SELECT * FROM create_hypertable('rpm_cont_data', 'recorded_at',chunk_time_interval => INTERVAL '1 week');
+SELECT * FROM create_hypertable('rpm_non_cont_data', 'recorded_at',chunk_time_interval => INTERVAL '1 month');
+
+INSERT INTO devices_meta(device_id, device_name, vendor,created_at, is_device_in_hub,  created_at_hub ) VALUES ('rpm-pbi-dev-1','PM6100','BerryMed','2020-09-28 18:46:28','1','2020-09-28 18:46:28');
+INSERT INTO devices_meta(device_id, device_name, vendor,created_at, is_device_in_hub,  created_at_hub ) VALUES ('rpm-pbi-dev-2','PM6100','BerryMed','2020-09-28 18:46:28','1','2020-09-28 18:46:28');
+INSERT INTO devices_meta(device_id, device_name, vendor,created_at, is_device_in_hub) VALUES ('rpm-pbi-dev-3','PM6100','BerryMed','2020-09-28 18:46:28','0');
+INSERT INTO devices_meta(device_id, device_name, vendor,created_at, is_device_in_hub) VALUES ('rpm-pbi-dev-4','PM6100','BerryMed','2020-09-28 18:46:28','0');
+INSERT INTO devices_meta(device_id, device_name, vendor,created_at, is_device_in_hub) VALUES ('rpm-pbi-dev-5','PM6100','BerryMed','2020-09-28 18:46:28','0');
+INSERT INTO devices_meta(device_id, device_name, vendor,created_at, is_device_in_hub) VALUES ('rpm-pbi-dev-6','PM6100','BerryMed','2020-09-28 18:46:28','0');
+UPDATE devices_meta SET created_at_hub = NULL WHERE device_id = 'rpm-pbi-dev-3';
+
+
+INSERT INTO users_meta(user_id, user_name, address, dob, created_at ) VALUES ('100001','Hussnain Ahmed','H8/4 Islamabad','1982-04-04','2020-09-28 18:46:28');
+INSERT INTO users_meta(user_id, user_name, address, dob, created_at ) VALUES ('100002','Ali Ahmed','H8/4 Islamabad','1982-04-04','2020-09-28 18:46:28');
+INSERT INTO users_meta(user_id, user_name, address, dob, created_at ) VALUES ('100003','Shehnaz Akhter','I8/3 Islamabad','1965-07-05','2020-09-28 18:46:28');
+INSERT INTO users_meta(user_id, user_name, address, dob, created_at ) VALUES ('100004','Shahid Hameed','F7/4 Islamabad','1982-09-08','2020-09-28 18:46:28');
+INSERT INTO users_meta(user_id, user_name, address, dob, created_at ) VALUES ('100005','Farhan Ali','H8/3 Islamabad','1976-05-03','2020-09-28 18:46:28');
+INSERT INTO users_meta(user_id, user_name, address, dob, created_at ) VALUES ('100006','Shazia Khan','H8/2 Islamabad','2001-03-04','2020-09-28 18:46:28');
 
 
 
 
-ALTER TABLE "device_rentals" ADD CONSTRAINT "device_rentals_fk0" FOREIGN KEY ("device_id") REFERENCES "devices_meta"("device_id");
-ALTER TABLE "device_rentals" ADD CONSTRAINT "device_rentals_fk1" FOREIGN KEY ("rented_to_uid") REFERENCES "users_meta"("user_id");
-ALTER TABLE "device_rentals" ADD CONSTRAINT "device_rentals_fk2" FOREIGN KEY ("rented_to_name") REFERENCES "users_meta"("user_name");
+INSERT INTO device_rentals (device_id, rented_at, rented_to_uid, rented_to_name ) VALUES ('rpm-pbi-dev-1','2020-09-28 18:46:28','100001','Hussnain Ahmed');
+INSERT INTO device_rentals (device_id, rented_at, rented_to_uid, rented_to_name ) VALUES ('rpm-pbi-dev-2','2020-09-28 18:46:28','100002','Ali Ahmed');
+INSERT INTO device_rentals (device_id, rented_at, rented_to_uid, rented_to_name ) VALUES ('rpm-pbi-dev-3','2020-09-28 18:46:28','100003','Shehnaz Akhter');
 
-ALTER TABLE "rpm_cont_data" ADD CONSTRAINT "rpm_cont_data_fk0" FOREIGN KEY ("device_id") REFERENCES "devices_meta"("device_id");
-ALTER TABLE "rpm_cont_data" ADD CONSTRAINT "rpm_cont_data_fk1" FOREIGN KEY ("rented_to_id") REFERENCES "users_meta"("user_id");
-ALTER TABLE "rpm_cont_data" ADD CONSTRAINT "rpm_cont_data_fk2" FOREIGN KEY ("rented_to_name") REFERENCES "users_meta"("user_name");
 
-ALTER TABLE "rpm_cont_data copy" ADD CONSTRAINT "rpm_cont_data copy_fk0" FOREIGN KEY ("device_id") REFERENCES "devices_meta"("device_id");
-ALTER TABLE "rpm_cont_data copy" ADD CONSTRAINT "rpm_cont_data copy_fk1" FOREIGN KEY ("rented_to_id") REFERENCES "users_meta"("user_id");
-ALTER TABLE "rpm_cont_data copy" ADD CONSTRAINT "rpm_cont_data copy_fk2" FOREIGN KEY ("rented_to_name") REFERENCES "users_meta"("user_name");
+
+INSERT INTO devices(id, type, rented_to, rented_at ) VALUES ('rpm-pbi-dev-4','PM6100','Ali','2020-09-28 18:46:28');
+INSERT INTO devices(id, type, rented_to, rented_at ) VALUES ('rpm-pbi-dev-5','PM6100','Wasim','2020-09-28 18:46:28');
+
 
